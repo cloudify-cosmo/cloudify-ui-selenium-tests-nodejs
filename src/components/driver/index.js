e0a3c239cdcc8195a14a624b5fa01ca2d5d2a469
guym <guym@gigaspaces.com>
2014-11-09 17:36:47 +0200

    adding selenium driver implementation.

--- src/components/driver/index.js
+++ src/components/driver/index.js
@@ -6,11 +6,29 @@
  **/
 
 
+/**
+ *
+ * @param opts {
+ *
+ *          'seleniumServerUrl' : 'http://localhost/wd/hub',
+ *          'rootUrl' : 'which url to load'
+ * }
+ */
 
-function generateDriver(){
+
+exports.generate = function( opts ){
+
+    var webdriver = require('selenium-webdriver');
+
+    var driver = new webdriver.Builder().
+        usingServer(opts.seleniumServerUrl).
+        withCapabilities(webdriver.Capabilities[opts.seleniumBrowserType]()).
+        build();
+    return driver;
+};
 
 
-}
+
 
 
 
--- tests/testSeleniumDriver.js
+++ tests/testSeleniumDriver.js
@@ -1,6 +1,19 @@
+var opts = {
+    'seleniumServerUrl': 'http://pc-lab58:4444/wd/hub',
+    'seleniumBrowserType': 'chrome'
+};
+var driver = require('../src/components/driver').generate( opts );
+
+driver.get('http://www.google.com');
+
+
+setTimeout(function () {
+
+    driver.quit();
+
+}, 10000);
 
 
 
-var driver = require('../src/component/driver').get();
 
 
