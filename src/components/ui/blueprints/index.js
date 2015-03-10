f3fd018dd635aeff21fa2a69d97fdd57a50575e2
itsikm <itsik@gigspaces.com>
2015-03-10 11:04:36 +0200

    validation of blueprint page

--- src/components/ui/blueprints/index.js
+++ src/components/ui/blueprints/index.js
@@ -4,6 +4,7 @@ var q = require('q');
 var logger = require('log4js').getLogger('Blueprints');
 var Upload = require('./Upload');
 var CreateDeployments = require('./CreateDeployment');
+var Page = require('./Page');
 
 /**
  * Create new instance of Upload
@@ -22,5 +23,15 @@ exports.newUploadInstance = function(){
 exports.createDeployment = function() {
     var deferred = q.defer();
     deferred.resolve(new CreateDeployments());
+    return deferred.promise;
+};
+
+/**
+ * Create new instance of Page
+ * @returns {promise}
+ */
+exports.page = function() {
+    var deferred = q.defer();
+    deferred.resolve(new Page());
     return deferred.promise;
 };
--- src/suites/sanity/testBlueprints.js
+++ src/suites/sanity/testBlueprints.js
@@ -37,6 +37,115 @@ describe('Blueprints', function () {
         });
     });
 
+    it('should have topology with 2 hosts', function(done){
+        // load blueprint page
+        components.ui.page.loadBlueprints();
+
+        // create new instance of blueprint page
+        components.ui.blueprints.page().then(function(page){
+            // select the first blueprint
+            page.selectByPosition(1).then(function(blueprintRow){
+                // click on the blueprint to get into the blueprint page
+                blueprintRow.click().then(function(){
+                    // indicate for loading the blueprint page
+                    page.getHeader().then(function(){
+                        // get list of topology hosts
+                        page.getTopologyHosts().then(function(hosts){
+                            assert.equal(hosts.length, 2);
+                            done();
+                        }).catch(function (e) {
+                            done(e);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('should have network topology with 2 networks', function(done){
+        // load blueprint page
+        components.ui.page.loadBlueprints();
+
+        // create new instance of blueprint page
+        components.ui.blueprints.page().then(function(page){
+            // select the first blueprint
+            page.selectByPosition(1).then(function(blueprintRow){
+                // click on the blueprint to get into the blueprint page
+                blueprintRow.click().then(function(){
+                    // indicate for loading the blueprint page
+                    page.getHeader().then(function(){
+                        // switch to Network screen
+                        page.switchTab('Network').then(function(){
+                            // get list of networks
+                            page.getNetworks().then(function(networks){
+                                assert.equal(networks.length, 2);
+                                done();
+                            }).catch(function (e) {
+                                done(e);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('should have total of 8 nodes', function(done){
+        // load blueprint page
+        components.ui.page.loadBlueprints();
+
+        // create new instance of blueprint page
+        components.ui.blueprints.page().then(function(page){
+            // select the first blueprint
+            page.selectByPosition(1).then(function(blueprintRow){
+                // click on the blueprint to get into the blueprint page
+                blueprintRow.click().then(function(){
+                    // indicate for loading the blueprint page
+                    page.getHeader().then(function(){
+                        // switch to Nodes screen
+                        page.switchTab('Nodes').then(function(){
+                            // get list of nodes
+                            page.getNodes().then(function(nodes){
+                                assert.equal(nodes.length, 8);
+                                done();
+                            }).catch(function (e) {
+                                done(e);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('should have 45 folders and files on the source tree', function(done){
+        // load blueprint page
+        components.ui.page.loadBlueprints();
+
+        // create new instance of blueprint page
+        components.ui.blueprints.page().then(function(page){
+            // select the first blueprint
+            page.selectByPosition(1).then(function(blueprintRow){
+                // click on the blueprint to get into the blueprint page
+                blueprintRow.click().then(function(){
+                    // indicate for loading the blueprint page
+                    page.getHeader().then(function(){
+                        // switch to Source screen
+                        page.switchTab('Source').then(function(){
+                            // get list of folders and files on the tree
+                            page.getSourceTree().then(function(tree){
+                                assert.equal(tree.length, 45);
+                                done();
+                            }).catch(function (e) {
+                                done(e);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
     it('should create new deployment', function(done){
         // load blueprint page
         components.ui.page.loadBlueprints();
