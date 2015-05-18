914ab130b4479830ca749355291d35301a4c744b
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-05-18 17:51:51 +0300

    use cloudify installer instead of scripting the bootstrap

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -1,3 +1,9 @@
 cd /vagrant
-./run_bootstrap.sh
+./install_prereq.sh
+
+## bootstrap
+
+sudo npm -g install cloudify-cosmo/cloudify-installer --ignore-scripts
+cloudify-installer run_script -s 3.2.0/vagrant_install_simple/script.sh
+
 ./run_test.sh
