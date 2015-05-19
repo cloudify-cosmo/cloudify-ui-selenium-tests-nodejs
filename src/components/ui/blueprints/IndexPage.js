3f2c7abb5e8baa9ede58752f7249bc9a4b001f3a
erezcarmel <erezcarmel@gmail.com>
2015-05-19 17:10:22 +0300

    CFY-2161 Fixed blueprints tests

--- src/components/ui/blueprints/IndexPage.js
+++ src/components/ui/blueprints/IndexPage.js
@@ -28,16 +28,15 @@ exports.getBlueprint = function( opts ){
     logger.trace('getting blueprint by ', opts );
     var deferred = protractor.promise.defer();
     exports.getBlueprints().filter(function(blueprint){
-           return blueprint.element(by.css('.name')).getText().then(function( text ){
-               return text === opts.name;
-           });
-        }).then(function(filtered){
-            deferred.fulfill(filtered[0]);
+        return blueprint.element(by.css('.name')).getText().then(function( text ){
+            return text === opts.name;
         });
+    }).then(function(filtered){
+        deferred.fulfill(filtered[0]);
+    });
 
     return deferred.promise;
 };
-
 
 exports.goToBlueprint = function( opts ){
     return exports.getBlueprint(opts).then(function(blueprint){
