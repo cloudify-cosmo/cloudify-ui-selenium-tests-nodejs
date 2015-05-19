94cfbd5b018e92b7c883c3412ce2087dd779a5b4
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-05-19 18:36:49 +0300

    updating apt-get before installing prepreq

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -1,11 +1,12 @@
 cd /vagrant
+sudo apt-get update -y
 ./install_prereq.sh
 
 ## bootstrap
 
 sudo npm -g install cloudify-cosmo/cloudify-installer --ignore-scripts
 export INSTALL_SYSTEM_TESTS_REQ=true
-sudo apt-get update -y
+
 cloudify-installer run_script -s 3.2.0/vagrant_install_simple/script.sh
 
 ./run_test.sh
