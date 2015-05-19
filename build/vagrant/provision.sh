91dde49d4cc6a4af62bf09625ef82b3537be77f9
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-05-19 16:17:58 +0300

    add install_system_test_req flag to cloudify-installer

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -4,6 +4,8 @@ cd /vagrant
 ## bootstrap
 
 sudo npm -g install cloudify-cosmo/cloudify-installer --ignore-scripts
+export INSTALL_SYSTEM_TESTS_REQ=true
+sudo apt-get update -y
 cloudify-installer run_script -s 3.2.0/vagrant_install_simple/script.sh
 
 ./run_test.sh
