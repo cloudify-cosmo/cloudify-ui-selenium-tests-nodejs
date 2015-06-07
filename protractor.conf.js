99a78a76b5a94641a942f11195a25e22b6c381a7
sefigiga <gigaS3f1nbar>
2015-06-07 11:17:37 +0300

    protractor conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -16,8 +16,7 @@ exports.config = {
 
     // A base URL for your application under test. Calls to protractor.get()
     // with relative paths will be prepended with this.
-    baseUrl: process.env.PROTRACTOR_BASE_URL || 'http://192.168.41.111',
-    //baseUrl: process.env.PROTRACTOR_BASE_URL || 'http://127.0.0.1',
+    baseUrl: process.env.PROTRACTOR_BASE_URL || 'http://cloudify.localhost.com',
 
     jasmineNodeOpts: {
         onComplete: null,
