debe6037a3b418d822ad61c080c5d67baa1044f8
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-07-09 18:42:44 +0300

    change resolution for applitools

--- spec_design/example_spec.js
+++ spec_design/example_spec.js
@@ -1,4 +1,7 @@
 'use strict';
+
+// we test on 1920 x 1080 by default
+
 var Eyes = require('eyes.protractor').Eyes;
 var eyes = new Eyes();
 eyes.setApiKey(process.env.APPLITOOLS_KEY);
@@ -6,7 +9,7 @@ eyes.setApiKey(process.env.APPLITOOLS_KEY);
 describe('cloudify-ui blueprint', function() {
     it('shows blueprints', function() {
         eyes.setMatchLevel('Layout');
-        eyes.open(browser, 'Cloudify UI', 'Sanity Suite', {width: 800, height: 600});
+        eyes.open(browser, 'Cloudify UI', 'Sanity Suite', {width: 1920, height: 1080});
 
         browser.get('/');
         eyes.checkWindow('Blueprints Page');
