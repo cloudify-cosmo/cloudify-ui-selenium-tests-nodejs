a6437b5446b1e726c88baaa607e510f91a74af32
Eden <eden@gigaspaces.com>
2015-08-16 12:04:14 +0300

    feat(login page applitools)
    added login page to applitools
    
    CFY-2967

--- spec_design/example_spec.js
+++ spec_design/example_spec.js
@@ -6,15 +6,24 @@ var Eyes = require('eyes.protractor').Eyes;
 var eyes = new Eyes();
 eyes.setApiKey(process.env.APPLITOOLS_KEY);
 
-describe('cloudify-ui blueprint', function() {
-    it('shows blueprints', function() {
+describe('cloudify-ui layout', function(){
+    beforeEach(function(){
         eyes.setMatchLevel('Layout');
-        eyes.open(browser, 'Cloudify UI', 'Sanity Suite', {width: 1920, height: 1080});
+    });
 
-        browser.get('/');
-        eyes.checkWindow('Blueprints Page');
-
+    afterEach(function(){
         eyes.close();
     });
 
-});
+    it('shows blueprints', function() {
+        eyes.open(browser, 'Cloudify UI', 'Blueprints Suite', {width: 1920, height: 1080});
+        browser.get('/');
+        eyes.checkWindow('Blueprints Page');
+    });
+
+    it('checks login layout', function(){
+        eyes.open(browser, 'Cloudify UI', 'Login Suite', {width: 1920, height: 1080});
+        browser.get('/#/login');
+        eyes.checkWindow('Login Page');
+    });
+})
