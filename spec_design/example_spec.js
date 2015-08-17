b98c664533c0cba5a507d9319a3281cd2d7e78bc
Eden <eden@gigaspaces.com>
2015-08-17 14:12:19 +0300

    feat(login page applitools)
    added login page to applitools
    
    CFY-2967

--- spec_design/example_spec.js
+++ spec_design/example_spec.js
@@ -7,23 +7,43 @@ var eyes = new Eyes();
 eyes.setApiKey(process.env.APPLITOOLS_KEY);
 
 describe('cloudify-ui layout', function(){
-    beforeEach(function(){
-        eyes.setMatchLevel('Layout');
-    });
+    var init = false;
 
-    afterEach(function(){
-        eyes.close();
+    function isInitialized()
+    {
+        if(!init){
+            init = true;
+            return false;
+        }
+        else{
+            return true
+        }
+    }
+
+    beforeEach(function(){
+        if(!isInitialized()){
+            eyes.setMatchLevel('Layout');
+            eyes.open(browser, 'Cloudify UI', 'Sanity Suite', {width: 1920, height: 1080});
+        }
     });
 
     it('shows blueprints', function() {
-        eyes.open(browser, 'Cloudify UI', 'Blueprints Suite', {width: 1920, height: 1080});
         browser.get('/');
         eyes.checkWindow('Blueprints Page');
     });
 
     it('checks login layout', function(){
-        eyes.open(browser, 'Cloudify UI', 'Login Suite', {width: 1920, height: 1080});
         browser.get('/#/login');
         eyes.checkWindow('Login Page');
     });
-})
+
+
+
+
+    //______________________________________________________________________________________________________________________________________
+    //                                                    ~~~~~~~~ ATTENTION ~~~~~~~~
+    //We have no indication of how to know when the last test ran, so remember to keep this test the last of all test so it could close eyes
+    it('should close Applitools eyes', function(){
+        eyes.close();
+    });
+});
