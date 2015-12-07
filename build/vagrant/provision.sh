79741b70845caddc9d3945eea9255c49695e1c5e
Eden <eden@gigaspaces.com>
2015-12-07 14:32:10 +0200

    refactor(provision) remove unnecessary defaults, so will not override parameters CFY-4269

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -12,9 +12,9 @@ cd /vagrant
 source /etc/ENVIRONMENT_VARIABLES.sh || echo "no environment variables file.. skipping.. "
 sudo npm -g install cloudify-cosmo/cloudify-installer#master --ignore-scripts
 
-
-export INSTALL_SYSTEM_TESTS_REQ=true
-export TAG="master"
+if [ "$INSTALL_SYSTEM_TESTS_REQ" = "" ]; then
+    export INSTALL_SYSTEM_TESTS_REQ="true"
+fi
 cloudify-installer run_script -s 3.3.0/vagrant_install_simple/script.sh
 
 ./run_test.sh
