c96bf7b149373f4fda10d86fff1dabfbf2caa951
Eden <eden@gigaspaces.com>
2015-12-13 14:42:05 +0200

    refactor(provision) remove unnecessary defaults, so will not override parameters CFY-4269

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -2,11 +2,6 @@ set -e
 set -v
 set -x
 
-echo "$1 is "$1
-echo "$2 is "$2
-echo "$TAG is "$TAG
-echo "$TYPE is "$TYPE
-
 export NO_COLOR="--no-color"
 
 cd /vagrant
@@ -17,10 +12,11 @@ cd /vagrant
 source /etc/ENVIRONMENT_VARIABLES.sh || echo "no environment variables file.. skipping.. "
 sudo npm -g install cloudify-cosmo/cloudify-installer#master --ignore-scripts
 
+# preparation for overriding this value
 if [ "$INSTALL_SYSTEM_TESTS_REQ" = "" ]; then
     export INSTALL_SYSTEM_TESTS_REQ="true"
 fi
-echo "TAG is "$TAG
+
 cloudify-installer run_script -s 3.3.0/vagrant_install_simple/script.sh
 
 ./run_test.sh
