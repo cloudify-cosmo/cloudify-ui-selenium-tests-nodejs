8c94951bb48a9b66a6516405b2c8832d8b6e2ebd
Eden <eden@gigaspaces.com>
2015-12-14 14:20:58 +0200

    refactor(provision) supporting passing variables- automation+dev CFY-4269

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -2,10 +2,6 @@ set -e
 set -v
 set -x
 
-source /etc/ENVIRONMENT_VARIABLES.sh || echo "no environment variables file.. skipping.. "
-echo "Provision TAG is "$TAG
-echo "Provision TYPE is "$TYPE
-
 export NO_COLOR="--no-color"
 
 cd /vagrant
@@ -13,14 +9,10 @@ cd /vagrant
 ./install_prereq.sh
 
 ## bootstrap
-source /etc/ENVIRONMENT_VARIABLES.sh || echo "no environment variables file.. skipping.. "
+source /etc/ENVIRONMENT_VARIABLES.sh || source synced_folder/dev/ENVIRONMENT_VARIABLES.sh || echo "no environment variables file.. skipping.. "
 sudo npm -g install cloudify-cosmo/cloudify-installer#master --ignore-scripts
 
-# preparation for overriding this value
-if [ "$INSTALL_SYSTEM_TESTS_REQ" = "" ]; then
-    export INSTALL_SYSTEM_TESTS_REQ="true"
-fi
-
+export INSTALL_SYSTEM_TESTS_REQ="true"
 cloudify-installer run_script -s 3.3.0/vagrant_install_simple/script.sh
 
 ./run_test.sh
