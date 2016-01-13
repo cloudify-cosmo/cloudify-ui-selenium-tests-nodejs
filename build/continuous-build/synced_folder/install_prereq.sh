1b630865a722b8ea00404ceb682ff0493a9d73ba
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-01-13 15:00:00 +0200

    try to fix nvm loading

--- build/continuous-build/synced_folder/install_prereq.sh
+++ build/continuous-build/synced_folder/install_prereq.sh
@@ -7,10 +7,14 @@ set -e
 ##########
 
 echo "install nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
-source ~/.nvm/nvm.sh || . ~/.nvm/nvm.sh
 
-nvm install 0.10.35
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
+
+function load_nvm(){
+    echo "loading nvm"
+    source ~/.nvm/nvm.sh || . ~/.nvm/nvm.sh
+}
+
 
 # for phantomjs https://github.com/Medium/phantomjs/issues/92
 # https://www.centos.org/forums/viewtopic.php?t=21181
@@ -30,6 +34,7 @@ pushd ${PROJECT_PARENT_DIR}
     echo "clone finished"
     pushd ${GIT_DEST}
         echo "installing nvm `which nvm`"
+        load_nvm
         nvm install
         nvm install &> /dev/null
         echo "running npm install"
