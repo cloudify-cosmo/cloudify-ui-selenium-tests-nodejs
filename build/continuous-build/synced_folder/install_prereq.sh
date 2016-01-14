ce81f6edaee32453645f065a7360bc9f02a1d52e
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-01-14 11:16:51 +0200

    fix installations

--- build/continuous-build/synced_folder/install_prereq.sh
+++ build/continuous-build/synced_folder/install_prereq.sh
@@ -6,45 +6,39 @@ set -e
 #
 ##########
 
-echo "install nvm"
+# for phantomjs https://github.com/Medium/phantomjs/issues/92
+# https://www.centos.org/forums/viewtopic.php?t=21181
+echo "install bzip2, gcc, git and more.."
+sudo yum install -y bzip2 gcc-c++ git &> /dev/null
 
+echo "install nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
 source ~/.nvm/nvm.sh || . ~/.nvm/nvm.sh
 
 
-# for phantomjs https://github.com/Medium/phantomjs/issues/92
-# https://www.centos.org/forums/viewtopic.php?t=21181
-sudo yum install -y bzip2 gcc-c++
 
-
-if [ ! -f /usr/bin/git ]; then
-    echo "installing git"
-    sudo yum install -y git
-else
-    echo "git already installed"
-fi
 
 pushd ${PROJECT_PARENT_DIR}
     rm -rf ${PROJECT_NAME}
+    echo "clone project"
     git clone ${GIT_URL} ${GIT_DEST}
-    echo "clone finished"
     pushd ${GIT_DEST}
-        echo "installing nvm `which nvm`"
-        source ~/.nvm/nvm.sh || . ~/.nvm/nvm.sh
-        (nvm install || echo "unable to install nvm") &> /dev/null
-        echo "running npm install"
-        npm install
-        echo "installing build helper"
+        echo "install nodejs"
+        ls -ll
+        nvm install &> /dev/null  || echo "unable to run nvm install"
+        echo "install npm dependencies"
+        npm install &> /dev/null
+        echo "install build helper"
         npm -g install guy-mograbi-at-gigaspaces/cloudify-ui-build-helper
-        echo "creating tag"
-        create-and-push-git-tag
+        echo "create tag"
+        create-and-push-build-tag
     popd
 popd
 
 
 if [ ! -f /usr/bin/java ]; then
     echo "installing java"
-    sudo yum install -y java-1.7.0-openjdk
+    sudo yum install -y java-1.7.0-openjdk &> /dev/null
 #    sudo apt-get install -y openjdk-7-jre-headless
 else
     echo "java already installed"
@@ -52,7 +46,7 @@ fi
 
 if [ ! -f /usr/bin/grunt ]; then
     echo "installing grunt and phantom"
-    npm install -g grunt-cli phantomjs
+    npm install -g grunt-cli phantomjs &> /dev/null
 
 else
     echo "grunt and phantom already installed"
