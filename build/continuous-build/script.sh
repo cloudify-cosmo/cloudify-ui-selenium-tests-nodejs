6cf2285f989925d563644b049da0a1da3555a750
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-02-19 09:42:23 +0200

    fix url to custom ui

--- build/continuous-build/script.sh
+++ build/continuous-build/script.sh
@@ -31,7 +31,7 @@ if [ "${USE_UNSTABLE_UI}" = "true" ];then
     echo "using unstable ui url"
     npm install -g guy-mograbi-at-gigaspaces/cloudify-ui-build-helper
     source get-artifacts-files
-    CLOUDIFY_INSTALLER_INPUT_WEBUI_SOURCE_URL="https://s3.amazonaws.com/cloudify-ui/`PROJECT_NAME=cloudify-ui get-unstable-s3-folder`/${CLOUDIFY_UI_TAR_GZ}'"
+    CLOUDIFY_INSTALLER_INPUT_WEBUI_SOURCE_URL="https://s3.amazonaws.com/cloudify-ui/`PROJECT_NAME=cloudify-ui get-unstable-s3-folder`/${CLOUDIFY_UI_TAR_GZ}"
     echo "using custom ui url ${CLOUDIFY_INSTALLER_INPUT_WEBUI_SOURCE_URL}"
     json -I -f ${CONFIG_FILE} -e "this.environmentVariables.CLOUDIFY_INSTALLER_INPUT_WEBUI_SOURCE_URL=\"${CLOUDIFY_INSTALLER_INPUT_WEBUI_SOURCE_URL}\"" #
 fi
